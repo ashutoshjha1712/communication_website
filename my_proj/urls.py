98f736810b2d70ab6de8c2847f576407e3f38cc3
ashutoshjha1712 <103728387+ashutoshjha1712@users.noreply.github.com>
2022-11-19 16:36:03 +0530

    Update urls.py

--- my_proj/urls.py
+++ my_proj/urls.py
@@ -26,7 +26,7 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
-#     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-#     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
    
 ]
